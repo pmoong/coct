a4bf6228fdb3b209fa0f02684702ced95733c326
heywb457 <heywb457@naver.com>
2019-07-24 11:00:50 +0900

    로그인 로그아웃

---
+++ src/com/cw/coc/member/controller/LoginServlet.java
@@ -0,0 +1,90 @@
+package com.cw.coc.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cw.coc.member.model.service.MemberService;
+import com.cw.coc.member.model.vo.Member;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	//로그인 처리를 위한 상수 필드 선언
+	public static final int LOGIN_OK = 1;
+	public static final int WRONG_PASSWORD = 0;
+	public static final int ID_NOT_EXIST = -1;	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
+		
+		System.out.println("userId(서블릿) : " + userId);
+		System.out.println("userPwd(서블릿) : " + userPwd);
+		
+		Member reqMember = new Member();
+		reqMember.setUserId(userId);
+		reqMember.setUserPwd(userPwd);
+		
+		Member loginUser = new MemberService().loginCheck(reqMember);
+		
+		String view = "";
+		if(loginUser.getStatus() == LOGIN_OK) {
+			view = "/coc/index.jsp";
+			
+			//request.setAttribute("loginUser", loginUser);
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", loginUser);
+			
+			response.sendRedirect(view);
+			
+		}else {
+			view = "/views/common/errorPage.jsp";
+			
+			String msg;
+			if(loginUser.getStatus() == WRONG_PASSWORD) {
+				msg = "패스워드를 잘못 입력 하셨습니다.";
+			}else {
+				msg = "존재하지 않는 아이디입니다.";
+			}
+			
+			request.setAttribute("msg", msg);
+			
+			RequestDispatcher rd = request.getRequestDispatcher(view);
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/com/cw/coc/member/model/dao/MemberDao.java
+++ src/com/cw/coc/member/model/dao/MemberDao.java
@@ -6,11 +6,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Properties;
 
+import com.cw.coc.member.controller.LoginServlet;
 import com.cw.coc.member.model.vo.Member;
+import static com.cw.coc.common.JDBCTemplate.*;
 
 public class MemberDao {
 	private Properties prop = new Properties();
@@ -20,177 +20,90 @@ public class MemberDao {
 		
 		try {
 			prop.load(new FileReader(fileName));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	public int checkStatus(Connection con, Member reqMember) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		ResultSet rset = null;
+		
+		String query = prop.getProperty("checkStatus");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, reqMember.getUserId());
+			
+			rset = pstmt.executeQuery();
+			
+			String userId = "";
+			String userPwd = "";
+			
+			if(rset.next()) {
+				userId = rset.getString("USER_ID");
+				userPwd = rset.getString("USER_PWD");
+			}
+			
+			if(reqMember.getUserId().equals(userId) &&
+					reqMember.getUserPwd().equals(userPwd)) {
+				result = LoginServlet.LOGIN_OK;
+			}else if(reqMember.getUserId().equals(userId) &&
+					!reqMember.getUserPwd().equals(userPwd)) {
+				result = LoginServlet.WRONG_PASSWORD;
+			}else {
+				result = LoginServlet.ID_NOT_EXIST;
+			}
+			
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		
+		return result;
+	}
+
+	public Member selectOne(Connection con, Member reqMember) {
+		Member loginUser = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String query = prop.getProperty("selectOne");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, reqMember.getUserId());
+			pstmt.setString(2, reqMember.getUserPwd());
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				loginUser = new Member();
+				
+				loginUser.setUserId(rset.getString("USER_ID"));
+				loginUser.setUserPwd(rset.getString("USER_PWD"));
+				loginUser.setEmail(rset.getString("EMAIL"));
+				loginUser.setuType(rset.getString("UTYPE"));
+				loginUser.setGender(rset.getString("GENDER"));
+				loginUser.setAge(rset.getInt("AGE"));
+				loginUser.setSurvey(rset.getString("SCODE"));
+			}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		
+		return loginUser;
+	}
 	
-	//이하 회원 정보 조회용 메소드
-		public ArrayList<Member> selectAll(Connection con) {
-			ArrayList<Member> list = null;
-			Statement stmt = null;
-			ResultSet rset = null;
-			
-			String query = prop.getProperty("selectAll");
-
-			try {
-				stmt = con.createStatement();
-				
-				rset = stmt.executeQuery(query);
-				
-				list = new ArrayList<Member>();
-				
-				while(rset.next()) {
-					Member m = new Member();
-					
-					m.setUserNo(rset.getString("UNO"));
-					m.setUserId(rset.getString("USER_ID"));
-					m.setUserPwd(rset.getString("USER_PWD"));
-					m.setEmail(rset.getString("EMAIL"));
-					m.setClassify(rset.getString("CLASSIFY"));
-					m.setAge(rset.getInt("AGE"));
-					m.setGender(rset.getString("GENDER"));
-					m.setSurveyCode(rset.getString("SURVEY_CODE"));
-					
-					
-					
-					list.add(m);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close(stmt);
-				close(rset);
-			}
-			
-			
-			
-			return list;
-		}
-
-		public ArrayList<Member> searchId(Connection con, String searchValue) {
-			ArrayList<Member> list = null;
-			PreparedStatement pstmt = null;
-			ResultSet rset = null;
-			
-			String query = prop.getProperty("searchId");
-			
-			try {
-				pstmt = con.prepareStatement(query);
-				pstmt.setString(1, searchValue);
-				
-				rset = pstmt.executeQuery();
-				
-				list = new ArrayList<Member>();
-				
-				while(rset.next()) {
-					Member m = new Member();
-					
-					m.setUserNo(rset.getString("UNO"));
-					m.setUserId(rset.getString("USER_ID"));
-					m.setUserPwd(rset.getString("USER_PWD"));
-					m.setEmail(rset.getString("EMAIL"));
-					m.setClassify(rset.getString("CLASSIFY"));
-					m.setAge(rset.getInt("AGE"));
-					m.setGender(rset.getString("GENDER"));
-					m.setSurveyCode(rset.getString("SURVEY_CODE"));
-					
-					
-					list.add(m);
-				}
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close(pstmt);
-				close(rset);
-				
-			}
-			
-			return list;
-		}
-
-		public ArrayList<Member> searchName(Connection con, String searchValue) {
-			ArrayList<Member> list = null;
-			PreparedStatement pstmt = null;
-			ResultSet rset = null;
-			
-			String query = prop.getProperty("searchName");
-			
-			try {
-				pstmt = con.prepareStatement(query);
-				pstmt.setString(1, searchValue);
-				
-				rset = pstmt.executeQuery();
-				
-				list = new ArrayList<Member>();
-				
-				while(rset.next()) {
-					Member m = new Member();
-					
-					m.setUserNo(rset.getString("UNO"));
-					m.setUserId(rset.getString("USER_ID"));
-					m.setUserPwd(rset.getString("USER_PWD"));
-					m.setEmail(rset.getString("EMAIL"));
-					m.setClassify(rset.getString("CLASSIFY"));
-					m.setAge(rset.getInt("AGE"));
-					m.setGender(rset.getString("GENDER"));
-					m.setSurveyCode(rset.getString("SURVEY_CODE"));
-					
-					
-					list.add(m);
-				}
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close(pstmt);
-				close(rset);
-				
-			}
-			
-			return list;
-		}
-
-		public ArrayList<Member> searchGender(Connection con, String searchValue) {
-			ArrayList<Member> list = null;
-			PreparedStatement pstmt = null;
-			ResultSet rset = null;
-			
-			String query = prop.getProperty("searchGender");
-			
-			try {
-				pstmt = con.prepareStatement(query);
-				pstmt.setString(1, searchValue);
-				
-				rset = pstmt.executeQuery();
-				
-				list = new ArrayList<Member>();
-				
-				while(rset.next()) {
-					Member m = new Member();
-					
-					m.setUserNo(rset.getString("UNO"));
-					m.setUserId(rset.getString("USER_ID"));
-					m.setUserPwd(rset.getString("USER_PWD"));
-					m.setEmail(rset.getString("EMAIL"));
-					m.setClassify(rset.getString("CLASSIFY"));
-					m.setAge(rset.getInt("AGE"));
-					m.setGender(rset.getString("GENDER"));
-					m.setSurveyCode(rset.getString("SURVEY_CODE"));
-					
-					
-					list.add(m);
-				}
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close(pstmt);
-				close(rset);
-				
-			}
-			
-			return list;
-		}
 }
