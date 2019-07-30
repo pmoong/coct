2b56a13bf00b33f84b6703d4021e1bd716bc59c9
HwangMinHa0616 <49854733+minhaHwnag@users.noreply.github.com>
2019-07-30 13:37:18 +0900

    Revert "숙소 검색결과...입니다..."

--- src/com/cw/coc/place/model/service/PlaceService.java
+++ src/com/cw/coc/place/model/service/PlaceService.java
@@ -1,28 +1,5 @@
-package com.cw.coc.place.model.service;
-
-
-import static com.cw.coc.common.JDBCTemplate.close;
-import static com.cw.coc.common.JDBCTemplate.getConnection;
-
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import com.cw.coc.place.model.dao.PlaceDao;
-
-
+package com.cw.coc.place.model.sevice;
 
 public class PlaceService {
-
-	public ArrayList<HashMap<String, Object>> searchRoomList() {
-		Connection con = getConnection();
-
-		ArrayList<HashMap<String, Object>> list = new PlaceDao().searchRoomList(con);
-
-		close(con);
-
-		return list;	
-	}
-
 
 }
