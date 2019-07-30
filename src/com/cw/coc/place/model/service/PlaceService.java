2a393265bbe1f59fd80b3939c478d47b56dc99f6
kjs3124 <csh4723@naver.com>
2019-07-30 14:21:18 +0900

    숙소 검색 결과........

--- src/com/cw/coc/place/model/service/PlaceService.java
+++ src/com/cw/coc/place/model/service/PlaceService.java
@@ -1,5 +1,28 @@
-package com.cw.coc.place.model.sevice;
+package com.cw.coc.place.model.service;
+
+
+import static com.cw.coc.common.JDBCTemplate.close;
+import static com.cw.coc.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.cw.coc.place.model.dao.PlaceDao;
+
+
 
 public class PlaceService {
+
+	public ArrayList<HashMap<String, Object>> searchRoomList() {
+		Connection con = getConnection();
+
+		ArrayList<HashMap<String, Object>> list = new PlaceDao().searchRoomList(con);
+
+		close(con);
+
+		return list;	
+	}
+
 
 }
