e32140572241b12a7fdf8409717faf69ce16df87
kjs3124 <csh4723@naver.com>
2019-07-30 13:30:20 +0900

    숙소 검색결과...입니다...

---
+++ src/com/cw/coc/place/controller/SearchRoomServlet.java
@@ -0,0 +1,70 @@
+package com.cw.coc.place.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cw.coc.place.model.service.PlaceService;
+
+
+
+
+
+/**
+ * Servlet implementation class SearchRoomServlet
+ */
+@WebServlet("/searchRoom")
+public class SearchRoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchRoomServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("servlet ? ");
+		
+		ArrayList<HashMap<String, Object>> list = new PlaceService().searchRoomList();
+		
+		String locationName = request.getParameter("locationName");
+		System.out.println("locationName::"+locationName);
+		
+		
+		String page = "";
+		if(list != null) {
+			page = "views/place/searchRoom.jsp";
+			request.setAttribute("list", list);
+		}else {
+			page = "views/common/errorPage.jsp";
+			request.setAttribute("msg", "뭐야!");
+		}
+
+		request.getRequestDispatcher(page).forward(request, response);
+
+	}
+
+
+
+/**
+ * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+ */
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	doGet(request, response);
+}
+
+}
